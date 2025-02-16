b2d41912d1fe8dcf3b0d1784d35085d49c00b493
Vijay Gaikwad <vijaygkd@gmail.com>
2025-02-15 22:03:16 -0800

    reset rename

--- gpt2.py
+++ gpt2.py
@@ -177,15 +177,70 @@ class GPT(nn.Module):
 
         return model
 
+# -----------------------------------------------------------------------------
+import os
+import tiktoken
+import numpy as np
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32) # added after video
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+class DataLoaderLite:
+    def __init__(self, B, T, process_rank, num_processes, split):
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+        assert split in {'train', 'val'}
+
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
+
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T * self.num_processes
+        # if loading the next batch would be out of bounds, advance to next shard
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+        return x, y
+
 
 # ----------------------------------------------------------------------#
 num_return_sequences = 5
 max_length = 30
 
 # model = GPT.from_pretrained('gpt2')
+
 model = GPT(GPTConfig())    # randomly initialized model
 model.eval()
 model.to('mps')
+
+
+
 
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
@@ -199,7 +254,7 @@ torch.manual_seed(42)
 while x.size(1) < max_length: # max_length=30
     # forward the model to get the logits
     with torch.no_grad():
-        logits = model(x)[0] # (B, T, vocab_size)
+        logits, loss = model(x)[0] # (B, T, vocab_size)
         # take the logits at the last position
         logits = logits[:, -1, :] # (B, vocab_size)
         # get the probabilities
