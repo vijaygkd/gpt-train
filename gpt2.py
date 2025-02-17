13cf1147d8272d675645c7498eb7baaca2b7bb80
Vijay Gaikwad <vijaygkd@gmail.com>
2025-02-16 23:10:30 -0800

    Apply optimizations to speed up computation by 10x (#4)
    Speed test:
    
    GPU vanila | dt: 379.91ms | tok/sec: 43126.52
    TF32 | dt: 170.49ms | tok/sec: 96098.66
    BF16 mixed precision | dt: 161.31ms | tok/sec: 101567.53
    Torch compile | dt: 58.22ms | tok/sec: 281415.76
    Flash attention | dt: 43.76ms | tok/sec: 374403.29
    nice vocab size | dt: 40.78ms | tok/sec: 401729.68
    ~10x speed up!!!!
    380ms --> 40 ms
    43k tps --> 401k tps

--- gpt2.py
+++ gpt2.py
@@ -1,3 +1,4 @@
+import math
 from dataclasses import dataclass
 
 import torch
@@ -29,6 +30,7 @@ class CausalSelfAttention(nn.Module):
         # config
         self.n_head = config.n_head
         self.n_embd = config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
     
     def forward(self, x):
         B, T, C = x.size()  # batch_size, seq_len, n_embd
@@ -39,7 +41,16 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C//self.n_head).transpose(1,2)    # (B, nh, T, hs) # transpose n_head with seq_len for attention calculation
         k = k.view(B, T, self.n_head, C//self.n_head).transpose(1,2)    # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C//self.n_head).transpose(1,2)    # (B, nh, T, hs)
+        
+        # raw attention
+        # att = (q @ k.transpose(-2, -1)) / math.sqrt(k.size(-1))  # (B, nh, T, T)
+        # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        # att = F.softmax(att, dim=-1)
+        # y = att @ v  # (B, nh, T, hs)
+
+        # flash attention
         y = F.scaled_dot_product_attention(q, k, v, is_causal=True)     # flash attention # (B, nh, T, hs)
+
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side   # (B, T, C)
         # output projection
         y = self.c_proj(y)  # (B, T, C)
